0edaf1f165a473c3e14ffd1838da9f1e9ebae92b
haneul <khaneulc@kakao.com>
2025-02-24 17:34:02 +0900

    [feat] Repository Test 작성  - Brand/Category

---
+++ src/test/java/com/musinsa/catalog/hibernate/repository/BrandRepositoryTests.java
@@ -0,0 +1,120 @@
+package com.musinsa.catalog.hibernate.repository;
+
+import com.musinsa.catalog.hibernate.entity.BrandEntity;
+import com.musinsa.common.Code.YnType;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.TestConstructor.AutowireMode;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+@DataJpaTest
+@TestConstructor(autowireMode = AutowireMode.ALL)
+@RequiredArgsConstructor
+class BrandRepositoryTests {
+
+  private final BrandRepository brandRepository;
+  private final EntityManager entityManager;
+
+  private BrandEntity createBrand(String name) {
+    BrandEntity brand = BrandEntity.builder().name(name).build();
+    BrandEntity newBrand = brandRepository.save(brand);
+
+    log.info("Brand Created. (id: {})", brand.getId());
+
+    return newBrand;
+  }
+
+  @DisplayName("브랜드 저장 테스트")
+  @Test
+  void save() {
+    // Given, When
+    final String name = "name";
+    BrandEntity brand = createBrand(name);
+
+    // Then
+    Optional<BrandEntity> newBrand = brandRepository.findById(brand.getId());
+    assertThat(newBrand).isPresent();
+    assertThat(newBrand.get().getName()).isEqualTo(name);
+  }
+
+  @DisplayName("브랜드 이름 중복 오류 테스트")
+  @Test
+  void duplicate() {
+    // Given
+    final String name = "name";
+    BrandEntity brand = createBrand(name);
+
+    // When
+    Optional<BrandEntity> newBrand = brandRepository.findById(brand.getId());
+    BrandEntity duplicatedBrand = BrandEntity.builder().name(name).build();
+
+    // Then
+    assertThat(newBrand).isPresent();
+    assertThat(newBrand.get().getName()).isEqualTo(name);
+    assertThatThrownBy(() ->
+        brandRepository.save(duplicatedBrand)).isInstanceOf(DataIntegrityViolationException.class);
+  }
+
+  @DisplayName("브랜드 수정 테스트")
+  @Test
+  void update() {
+    // Given
+    BrandEntity brand = createBrand("name2");
+
+    // When
+    final String newName = "newName";
+    final String updatedId = "system";
+    brandRepository.updateBrandName(brand.getId(), newName, updatedId);
+    entityManager.clear();
+
+    // Then
+    Optional<BrandEntity> checkBrandUpdated = brandRepository.findById(brand.getId());
+    assertThat(checkBrandUpdated).isPresent();
+    assertThat(checkBrandUpdated.get().getName()).isEqualTo(newName);
+    assertThat(checkBrandUpdated.get().getUpdatedId()).isEqualTo(updatedId);
+  }
+
+  @DisplayName("브랜드 삭제 테스트")
+  @Test
+  void delete() {
+    // Given
+    BrandEntity brand = createBrand("name3");
+
+    // When
+    brandRepository.deleteById(brand.getId());
+
+    // Then
+    Optional<BrandEntity> checkBrandDeleted = brandRepository.findById(brand.getId());
+    assertThat(checkBrandDeleted).isEmpty();
+  }
+
+  @DisplayName("브랜드 삭제(deleteYn) 테스트")
+  @Test
+  void deleteYn() {
+    // Given
+    BrandEntity brand = createBrand("name3");
+
+    // When
+    final String updatedId = "system";
+    brandRepository.deleteAllById(List.of(brand.getId()), updatedId);
+    entityManager.clear();
+
+    // Then
+    Optional<BrandEntity> checkBrandDeleted = brandRepository.findById(brand.getId());
+    assertThat(checkBrandDeleted).isPresent();
+    assertThat(checkBrandDeleted.get().getDeleteYn()).isEqualTo(YnType.Y);
+    assertThat(checkBrandDeleted.get().getUpdatedId()).isEqualTo(updatedId);
+  }
+}
---
+++ src/test/java/com/musinsa/catalog/hibernate/repository/CategoryRepositoryTests.java
@@ -0,0 +1,141 @@
+package com.musinsa.catalog.hibernate.repository;
+
+import com.musinsa.catalog.hibernate.entity.CategoryEntity;
+import com.musinsa.common.Code.YnType;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.TestConstructor.AutowireMode;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+@DataJpaTest
+@TestConstructor(autowireMode = AutowireMode.ALL)
+@RequiredArgsConstructor
+class CategoryRepositoryTests {
+
+  private final CategoryRepository categoryRepository;
+  private final EntityManager entityManager;
+
+  private CategoryEntity createCategory(String code, String name) {
+    CategoryEntity category = CategoryEntity.builder().code(code).name(name).build();
+    CategoryEntity newCategory = categoryRepository.save(category);
+
+    log.info("Category Created. (id: {})", category.getId());
+
+    return newCategory;
+  }
+
+  @DisplayName("카테고리 저장 테스트")
+  @Test
+  void save() {
+    // Given, When
+    final String code = "code1";
+    final String name = "category1";
+    CategoryEntity category = createCategory(code, name);
+
+    // Then
+    Optional<CategoryEntity> newCategory = categoryRepository.findById(category.getId());
+    assertThat(newCategory).isPresent();
+    assertThat(newCategory.get().getCode()).isEqualTo(code);
+    assertThat(newCategory.get().getName()).isEqualTo(name);
+  }
+
+  @DisplayName("카테고리 코드 중복 오류 테스트")
+  @Test
+  void duplicate() {
+    // Given
+    final String code = "code1";
+    final String name = "category1";
+    CategoryEntity category = createCategory(code, name);
+
+    // When
+    Optional<CategoryEntity> newCategory = categoryRepository.findById(category.getId());
+    CategoryEntity duplicatedCategory = CategoryEntity.builder().code(code).name(name).build();
+
+    // Then
+    assertThat(newCategory).isPresent();
+    assertThat(newCategory.get().getCode()).isEqualTo(code);
+    assertThatThrownBy(() -> categoryRepository.save(duplicatedCategory)).isInstanceOf(DataIntegrityViolationException.class);
+  }
+
+  @DisplayName("카테고리 수정 테스트")
+  @Test
+  void update() {
+    // Given
+    final String code = "code2";
+    CategoryEntity category = createCategory(code, "category2");
+
+    // When
+    final String newName = "newCategory";
+    final String updatedId = "system";
+    categoryRepository.updateCategoryName(category.getId(), newName, updatedId);
+    entityManager.clear();
+
+    // Then
+    Optional<CategoryEntity> checkCategoryUpdated = categoryRepository.findById(category.getId());
+    assertThat(checkCategoryUpdated).isPresent();
+    assertThat(checkCategoryUpdated.get().getCode()).isEqualTo(code);
+    assertThat(checkCategoryUpdated.get().getName()).isEqualTo(newName);
+    assertThat(checkCategoryUpdated.get().getUpdatedId()).isEqualTo(updatedId);
+  }
+
+  @DisplayName("카테고리 삭제 테스트")
+  @Test
+  void delete() {
+    // Given
+    CategoryEntity category = createCategory("code3", "category3");
+
+    // When
+    categoryRepository.deleteById(category.getId());
+
+    // Then
+    Optional<CategoryEntity> checkCategoryDeleted = categoryRepository.findById(category.getId());
+    assertThat(checkCategoryDeleted).isEmpty();
+  }
+
+  @DisplayName("카테고리 삭제(deleteYn) 테스트")
+  @Test
+  void deleteYn() {
+    // Given
+    CategoryEntity category = createCategory("code3", "category3");
+
+    // When
+    final String updatedId = "system";
+    categoryRepository.deleteAllById(List.of(category.getId()), updatedId);
+    entityManager.clear();
+
+    // Then
+    Optional<CategoryEntity> checkCategoryDeleted = categoryRepository.findById(category.getId());
+    assertThat(checkCategoryDeleted).isPresent();
+    assertThat(checkCategoryDeleted.get().getDeleteYn()).isEqualTo(YnType.Y);
+    assertThat(checkCategoryDeleted.get().getUpdatedId()).isEqualTo(updatedId);
+  }
+
+  @DisplayName("카테고리 조회 테스트")
+  @Test
+  void select() {
+    // Given
+    final String code = "code3";
+    createCategory(code, "category3");
+
+    // When
+    Optional<CategoryEntity> category1 = categoryRepository.findByCode(code);
+    Optional<CategoryEntity> category2 = categoryRepository.findByCode("someCode");
+
+    // Then
+    assertThat(category1).isPresent();
+    assertThat(category1.get().getCode()).isEqualTo(code);
+    assertThat(category2).isEmpty();
+  }
+}
